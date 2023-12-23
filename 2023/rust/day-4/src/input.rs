0308977c984f18ba6857a803b2a8c9f114de2e2d
Alan Chodyniecki <alan.chody@gmail.com>
2023-12-23 15:22:43 +0000

    2023 rust day 4 refactor base

--- 2023/rust/day-4/src/input.rs
+++ 2023/rust/day-4/src/input.rs
@@ -1,5 +1,7 @@
 use std::{collections::HashSet, fs::File, io::{BufReader, BufRead}};
 
+use itertools::Itertools;
+
 use crate::ScratchCard;
 
 pub fn read_scratch_cards(filename: &str) -> Result<Vec<ScratchCard>, String> {
@@ -8,33 +10,25 @@ pub fn read_scratch_cards(filename: &str) -> Result<Vec<ScratchCard>, String> {
     let buffered_reader = BufReader::new(file);
     buffered_reader.lines()
         .enumerate()
-        .map(|(line_number, line_result)| 
-        line_result.map_err(|e| format!("Error reading line {}: {}", line_number, e))
-                .and_then(|line| parse_scratch_card(&line))
-                .map_err(|e| format!("Error parsing scratch card on line {}: {}", line_number, e))
-            
-        )
-        .collect()
+        .try_fold(Vec::new(), |mut acc, (line_number, line_result)| {
+            let line = line_result.map_err(|e| format!("Error reading line {}: {}", line_number, e))?;
+            let scratch_card = parse_scratch_card(&line)
+                .map_err(|e| format!("Error parsing scratch card on line {}: {}", line_number, e))?;
+            acc.push(scratch_card);
+            Ok(acc)
+        })
 }
 
 fn parse_scratch_card(line: &str) -> Result<ScratchCard, String> {
-    let mut card_description_and_numbers_strs = line.split(": ");
-
-    let card_description_str = card_description_and_numbers_strs.next()
-        .ok_or("Expected card description before ': ' separator")?;
-
-    let all_card_numbers_str = card_description_and_numbers_strs.next()
-        .ok_or("Expected card numbers after ': ' separator")?;
+    let (card_description_str, all_card_numbers_str) = line.splitn(2, ": ")
+        .collect_tuple()
+        .ok_or("Expected ': ' separator")?;
     
     let card_id = parse_card_id(card_description_str)?;
 
-    let mut winning_and_this_card_numbers_strs = all_card_numbers_str.split(" | ");
-
-    let winning_card_numbers_str = winning_and_this_card_numbers_strs.next()
-        .ok_or("Expected winning card numbers before ' | '")?;
-
-    let this_card_numbers_str = winning_and_this_card_numbers_strs.next()
-        .ok_or("Expected this card's numbers after ' | '")?;
+    let (winning_card_numbers_str, this_card_numbers_str) = all_card_numbers_str.splitn(2, " | ")
+        .collect_tuple()
+        .ok_or("Expected card numbers to be separated by ' | '")?;
 
     let winning_numbers = parse_whitespace_separated_numbers(winning_card_numbers_str)?;
 
@@ -48,21 +42,19 @@ fn parse_scratch_card(line: &str) -> Result<ScratchCard, String> {
 }
 
 fn parse_card_id(card_description_str: &str) -> Result<u32, String> {
-    let mut card_text_and_card_id = card_description_str.split_whitespace();
-
-    card_text_and_card_id.next();
-
-    let card_id_str = card_text_and_card_id.next()
-        .ok_or("Expected card id after 'Card '")?;
+    let (_, card_id_str) = card_description_str.split_whitespace()
+        .collect_tuple()
+        .ok_or("Expected 'Card {cardId}")?;
     card_id_str.parse()
         .map_err(|_: std::num::ParseIntError| format!("Invalid Card Id: {}", card_id_str))
 }
 
 fn parse_whitespace_separated_numbers(whitespace_separated_numbers_str: &str) -> Result<HashSet<u32>, String> {
     whitespace_separated_numbers_str.split_whitespace()
-        .map(|number| 
-            number.parse()
-                .map_err(|_| format!("Not a valid number: {}", number))
-        )
-        .collect()
+        .try_fold(HashSet::new(), |mut acc, number_str| {
+            let number = number_str.parse()
+                .map_err(|_| format!("Not a valid number: {}", number_str))?;
+            acc.insert(number);
+            Ok(acc)
+        })
 }
