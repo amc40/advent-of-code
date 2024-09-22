882bfd1c96e67ccc8a60c75e905dd6bee889d0d1
Alan Chodyniecki <alan.chody@gmail.com>
2024-09-22 18:30:16 +0000

    refactor input.rs

--- 2023/rust/day-5/src/input.rs
+++ 2023/rust/day-5/src/input.rs
@@ -34,9 +34,9 @@ pub fn read_input_from_file(filename: &str) -> Result<Input, String> {
     let mut mappings_by_source_name = HashMap::new();
 
     // skip over blank line before mappings
-    let skip_result = lines.next()
-        .ok_or("Expected at least one mapping")?;
-    skip_result.map_err(|e| format!("Error reading line to skip. {}", e))?;
+    lines.next()
+        .ok_or("Expected at least one mapping")?
+        .map_err(|e| format!("Error reading line to skip. {}", e))?;
 
     while let Some(mapping) = read_mapping(&mut lines)? {
         mappings_by_source_name.insert(mapping.source_name.clone(), mapping);
@@ -49,9 +49,8 @@ pub fn read_input_from_file(filename: &str) -> Result<Input, String> {
 }
 
 fn read_seeds_from_first_line(line_iterator: &mut Lines<BufReader<File>>) -> Result<Vec<u64>, String> {
-    let first_line_result = line_iterator.next()
-        .ok_or("There are no lines in the provided: {}")?;
-    let first_line = first_line_result
+    let first_line = line_iterator.next()
+        .ok_or("There are no lines in the provided: {}")?
         .map_err(|e| format!("Error reading line:\n{}", e))?;
 
     read_seeds_from_first_line_str(&first_line)
@@ -65,61 +64,47 @@ fn read_seeds_from_first_line_str(first_line: &str) -> Result<Vec<u64>, String> 
 }
 
 fn read_mapping(line_iterator: &mut Lines<BufReader<File>>) -> Result<Option<Mapping>, String> {
-    if let Some(line_result) = line_iterator.next() {
-        let line = line_result
-            .map_err(|e| format!("Error parsing line. {}", e))?;
+    if let Some(line) = line_iterator.next().transpose().map_err(|_| String::from("Error reading mapping header"))? {
         let (source_name, destination_name_with_suffix) = line.splitn(2, "-to-")
             .collect_tuple()
-            .ok_or(format!("Error splitting mapping start line:\n{}", line))?;
+            .ok_or_else(|| format!("Error splitting mapping start line:\n{}", line))?;
         let destination_name = destination_name_with_suffix.strip_suffix(" map:")
-            .ok_or(format!("Mapping start line does not end with ' map:'\n{}", line))?;
+            .ok_or_else(|| format!("Mapping start line does not end with ' map:'\n{}", line))?;
         
         let mapping_entries = read_mapping_entries(line_iterator)?;
-        return Ok(Some(Mapping {
+        Ok(Some(Mapping {
             source_name: source_name.to_string(),
             destination_name: destination_name.to_string(),
             mapping_entries
         }))
     } else {  
-        return Ok(None)
+        Ok(None)
     }
 }
 
-fn read_mapping_entries(line_iterator: &mut Lines<BufReader<File>>) -> Result<Vec<RangeMapEntry>, String> {
-    let line_result = line_iterator.next()
-        .ok_or(format!("There should be at least one range mapping"))?;
-    let mut line = line_result
-        .map_err(|e| format!("Error reading mapping line: {}", e))?;
+fn read_mapping_entries(lines: &mut Lines<BufReader<File>>) -> Result<Vec<RangeMapEntry>, String> {
     let mut mapping_entries = Vec::new();
-    while !line.is_empty() {
+    while let Some(line) = lines.next().transpose().map_err(|e| format!("Error reading mapping line: {}", e))? {
+        if line.is_empty() {
+            break;
+        }
         let numbers_in_line = parse_whitespace_separated_numbers(&line)?;
+
         if numbers_in_line.len() != 3 {
             return Err("Expected exactly 3 numbers in mapping entry".to_owned());
         }
-        let source_range_start = numbers_in_line[1];
-        let dest_range_start = numbers_in_line[0];
-        let length = numbers_in_line[2];
-        mapping_entries.push(RangeMapEntry {
-            source_start: source_range_start,
-            destination_start: dest_range_start,
-            length: length,
+         mapping_entries.push(RangeMapEntry {
+            source_start: numbers_in_line[1],
+            destination_start: numbers_in_line[0],
+            length: numbers_in_line[2],
         });
-        if let Some(line_result) = line_iterator.next() {
-            line = line_result
-                .map_err(|e| format!("Error reading mapping line: {}", e))?
-        } else {
-            return Ok(mapping_entries)
-        }
     }
     Ok(mapping_entries)
 }
 
 fn parse_whitespace_separated_numbers(whitespace_separated_numbers: &str) -> Result<Vec<u64>, String> {
     whitespace_separated_numbers.split_whitespace()
-        .try_fold(Vec::new(), |mut acc_vec, number_str| {
-            let number = number_str.parse()
-                .map_err(|_| format!("Not a valid number: {}", number_str))?;
-            acc_vec.push(number);
-            Ok(acc_vec)
-        })
+        .map(|num_str| num_str.parse()
+            .map_err(|_| format!("Not a valid number: {}", num_str)))
+        .collect()
 }
